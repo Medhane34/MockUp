28f4455946136096d135674fcca179d3cf4b58dc
Medhane34e <daniaregawi3@gmail.com>
2026-06-14 22:50:15 +0300

    sanity update

--- web/src/sanity/client.ts
+++ web/src/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-  dataset: process.env.SANITY_STUDIO_DATASET,
+  dataset: "production",
   token: process.env.SANITY_API_WRITE_TOKEN,
   apiVersion: "2026-05-05",
   useCdn: false,
